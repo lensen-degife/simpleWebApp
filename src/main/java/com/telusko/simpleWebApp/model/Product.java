2af26cca4672b5710fa2d8ec925b218d4e3bdccc
lensen-degife <lensendegife@gmail.com>
2026-05-10 00:59:07 -0700

    Initial commit

---
+++ src/main/java/com/telusko/simpleWebApp/model/Product.java
@@ -0,0 +1,58 @@
+package com.telusko.simpleWebApp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity
+public class Product {
+
+    @Id
+    private int prodId;
+    private String prodName;
+    private int price;
+
+    public Product(int price, String prodName, int prodId) {
+        this.prodName = prodName;
+        this.price = price;
+        this.prodId = prodId;
+    }
+
+    public Product() {
+
+    }
+
+    public int getProdId() {
+        return prodId;
+    }
+
+    public void setProdId(int prodId) {
+        this.prodId = prodId;
+    }
+
+    public String getProdName() {
+        return prodName;
+    }
+
+    public void setProdName(String prodName) {
+        this.prodName = prodName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "prodId=" + prodId +
+                ", prodName='" + prodName + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ src/main/java/com/telusko/simpleWebApp/service/ProductService.java
@@ -0,0 +1,52 @@
+package com.telusko.simpleWebApp.service;
+
+import com.telusko.simpleWebApp.model.Product;
+import com.telusko.simpleWebApp.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepo repo;
+
+//    List<Product> products = new ArrayList<>(Arrays.asList(
+//            new Product(101, "Iphone", 5000),
+//            new Product(102, "canon camera", 70000),
+//            new Product(103, "Shure mic", 40000)
+//    )
+//    );
+
+    public List<Product> getProducts() {
+
+        return repo.findAll();
+    }
+
+
+    public Product getProductById(int prodId) {
+        return repo.findById(prodId).orElse(new Product());
+    }
+
+
+    public void addProduct(Product prod) {
+        repo.save(prod);
+    }
+
+
+    public void updateProduct(Product prod) {
+        repo.save(prod);
+    }
+
+
+    public void deleteProduct(int prodId) {
+        repo.deleteById(prodId);
+
+    }
+}
